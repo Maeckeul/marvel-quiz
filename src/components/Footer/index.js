d227c2c7c5008d1ff4617d874d569b91d685b6c7
Maeckeul <cmaeckeul@gmail.com>
2021-06-26 18:51:36 -1000

    :tada: first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <div  className="footer-container">
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
